4a6c22a7600af9da9f07ea31c06b2da11351f524
Nayuki <13510543077@163.com>
2026-03-01 20:56:30 +0900

    Add files via upload

--- src/constants.py
+++ src/constants.py
@@ -3,45 +3,32 @@ from ..keyword_trigger import KeywordRoute, PermissionLevel
 
 _DEFAULT_KEYWORD_ROUTES: tuple[KeywordRoute, ...] = (
     KeywordRoute(keyword="今日老婆", action="draw_wife"),
-    KeywordRoute(keyword="jrlp", action="draw_wife"),
     KeywordRoute(keyword="抽老婆", action="draw_wife"),
-    
     KeywordRoute(keyword="我的老婆", action="show_history"),
-    KeywordRoute(keyword="wdlp", action="show_history"),
     KeywordRoute(keyword="抽取历史", action="show_history"),
-    
     KeywordRoute(keyword="强娶", action="force_marry"),
-    KeywordRoute(keyword="qiangqu", action="force_marry"),
-    
     KeywordRoute(keyword="关系图", action="show_graph"),
     KeywordRoute(keyword="羁绊图谱", action="show_graph"),
-    KeywordRoute(keyword="gxt", action="show_graph"),
-    
     KeywordRoute(keyword="rbq排行", action="rbq_ranking"),
-    KeywordRoute(keyword="rbqph", action="rbq_ranking"),
-    
     KeywordRoute(keyword="抽老婆帮助", action="show_help"),
     KeywordRoute(keyword="老婆插件帮助", action="show_help"),
-    KeywordRoute(keyword="clpbz", action="show_help"),
-
+    KeywordRoute(keyword="草nj", action="caonj"),
+    KeywordRoute(keyword="草nj排行", action="caonj_ranking"),
+    KeywordRoute(keyword="草nj关系图", action="caonj_graph"),
+    KeywordRoute(keyword="日群友", action="ri"),
+    KeywordRoute(keyword="日群友排行", action="ri_ranking"),
+    KeywordRoute(keyword="日群友关系图", action="ri_graph"),
+    KeywordRoute(keyword="nj体内", action="nj_body"),
+    KeywordRoute(keyword="好感度排行", action="affinity_ranking"),
+    KeywordRoute(keyword="好感度", action="affinity_query"),
+    KeywordRoute(keyword="恩爱排行", action="love_ranking"),
     KeywordRoute(
         keyword="重置记录",
         action="reset_records",
         permission=PermissionLevel.ADMIN,
     ),
     KeywordRoute(
-        keyword="czjl",
-        action="reset_records",
-        permission=PermissionLevel.ADMIN,
-    ),
-    
-    KeywordRoute(
         keyword="重置强娶时间",
-        action="reset_force_cd",
-        permission=PermissionLevel.ADMIN,
-    ),
-    KeywordRoute(
-        keyword="czqqsj",
         action="reset_force_cd",
         permission=PermissionLevel.ADMIN,
     ),
